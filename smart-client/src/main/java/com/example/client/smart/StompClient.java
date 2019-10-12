3ac3873a3bea11afbc7225d6cdb07716ad1de6df
Peter-Frank Spierenburg <spierepf@hotmail.com>
2019-10-12 12:24:52 -0300

    Forcing StompClient to attempt connection

--- smart-client/src/main/java/com/example/client/smart/StompClient.java
+++ smart-client/src/main/java/com/example/client/smart/StompClient.java
@@ -1,6 +1,7 @@
 package com.example.client.smart;
 
 import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
 
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.simp.stomp.StompSessionHandler;
@@ -16,14 +17,14 @@ public class StompClient {
 
     private static String URL = "ws://localhost:8080/spring-mvc-java/chat";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         WebSocketClient client = new StandardWebSocketClient();
         WebSocketStompClient stompClient = new WebSocketStompClient(client);
 
         stompClient.setMessageConverter(new MappingJackson2MessageConverter());
 
         StompSessionHandler sessionHandler = new MyStompSessionHandler();
-        stompClient.connect(URL, sessionHandler);
+        stompClient.connect(URL, sessionHandler).get();
 
         new Scanner(System.in).nextLine(); // Don't close immediately.
     }
